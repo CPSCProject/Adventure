ce1fc1110c8ab6cd2e6a782df0e285742ba6bdb2
erikafung <erikaef978@gmail.com>
2017-03-14 13:44:46 -0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,45 +1,31 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
-
-public class Test{
-  public static void main(String[] args){
-
-System.out.println("     ");
-System.out.println("     ");
-System.out.println("ROOM: KITCHEN");
-System.out.println("     ");
-
-
-
-
-KitchenTest r_testa = new KitchenTest(true,300);
-r_testa.startGame("yes");
-r_testa.difficultyKitchen("medium");
-boolean key2determinant1 = r_testa.KitchenExplore("2", "1" ,"345");
-System.out.println(key2determinant1);
-
-System.out.println("--------------------------------------------");
-System.out.println(" ");
-
-
-KitchenTest r_testb = new KitchenTest(true,300);
-r_testb.startGame("yes");
-r_testb.difficultyKitchen("medium");
-boolean key2determinant2 = r_testb.KitchenExplore("2", "1" ,"355");
-System.out.println(key2determinant2);
-
-System.out.println("--------------------------------------------");
-System.out.println(" ");
-
-
-KitchenTest r_testc = new KitchenTest(false,500);
-r_testc.startGame("no");
-r_testc.difficultyKitchen("hard");
-boolean key2determinant3 = r_testc.KitchenExplore("2", "1" ,"355");
-System.out.println(key2determinant3);
-
-
-}
+public class Test {
+  public static void main(String[] args) {
+    Key1autotest p1 = new Key1autotest(true, 2);
+    boolean rboolean, uboolean, eboolean, key1;
+    p1.startgamemethod("no");
+    p1.startgamemethod("yes");
+    p1.difficultymethod("easy");
+    p1.difficultymethod("medium");
+    p1.difficultymethod("hard");
+    rboolean = p1.puzzle1method("mailbox", "wholesome", "inkstand");
+    //test wrong answers: lives will become negative
+    p1.puzzle1method("mailboxs", "wholesome", "inkstand");
+    p1.puzzle1method("mailbox", "wholesomes", "inkstand");
+    p1.puzzle1method("mailbox", "wholesome", "inkstands");
+    uboolean = p1.puzzle2method("refunded", "observant", "flamingos");
+    //test wrong answers: lives will become negative
+    p1.puzzle2method("refundeds", "observant", "flamingos");
+    p1.puzzle2method("refunded", "observants", "flamingos");
+    p1.puzzle2method("refunded", "observant", "flamingoss");
+    eboolean = p1.puzzle4method("german");
+    //test wrong answer: lives will become negative
+    p1.puzzle4method("germans");
+    key1 = p1.key1control(eboolean, uboolean, rboolean);
+    if (key1) {
+      System.out.println("\nALL TESTS PASSED (8/8)");
+    }
+    else {
+      System.out.println("\nNOT ALL TESTS PASSED");
+    }
+  }
 }
