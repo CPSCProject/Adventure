e68a776654e7137167527f1e4dc08558ae348948
erikafung <erikaef978@gmail.com>
2017-03-19 11:26:50 -0600

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,12 +1,14 @@
 public class Test1 {
   public static void main(String[] args) {
-    key1 p1 = new key1();
-    boolean rboolean, uboolean, eboolean, key1;
+    //Key1 p1 = new Key1();
+    Key1text p1 = new Key1text();
+    boolean rboolean, uboolean, eboolean, cboolean, key1;
     p1.startgamemethod();
     p1.difficultymethod();
     rboolean = p1.puzzle1method();
     uboolean = p1.puzzle2method();
-    eboolean = p1.puzzle3method();
-    key1 = p1.key1control(eboolean, uboolean, rboolean);
+    cboolean = p1.puzzle3method();
+    eboolean = p1.puzzle4method();
+    key1 = p1.key1control(eboolean, cboolean, uboolean, rboolean);
   }
 }
