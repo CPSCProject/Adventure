423552dbc85f7ac847a74f02b13fa4de52f095dd
Jkhuang16 <jkhuang16@hotmail.com>
2017-03-24 13:56:02 -0600

    Add files via upload

---
+++ ButtonGrid.java
@@ -0,0 +1,273 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ButtonGrid extends JFrame implements ActionListener
+{
+
+
+	JFrame window;
+	JPanel p1;
+	JPanel p2;
+	JPanel p3;
+	JPanel p4;
+	JPanel p5;
+	JButton graphic;
+	JTextArea statusT;
+	JTextArea storyT;
+	JTextField input;
+	public JButton commandB1;
+	public JButton commandB2;
+	public JButton commandB3;
+	public JButton commandB4;
+	public JButton commandB5;
+	public JButton Image1, Image2, Image3, Image4;
+	Timer timer;
+
+    //the paths for the 10 different JPEG pictures (2 for each of the 5 rooms)
+    String kitchenPath = "kitchen.jpg";
+    String kitchenPathKey = "kitchen_key1.jpg";
+	String dungeonPath = "dungeon.jpg";
+    String dungeonPathKey = "dungeon_key2.jpg";
+    String boilerPath = "boiler.jpg";
+    String boilerPathKey = "boiler_key3.jpg";
+    String libraryPath = "library.jpg";
+    String libraryPathKey = "library_key4.jpg";
+    String bedroomPath = "bedroom.jpg";
+    String bedroomPathKey = "bedroom_key5.jpg";
+
+	private JLabel label;
+	private JButton button;
+	private JTextField textfield;
+
+	public static void main(String[]args)
+	{
+
+		ButtonGrid game = new ButtonGrid();
+		game.opening();
+
+	}
+
+	public void opening()
+	{
+
+		JFrame window = new JFrame();
+		window.setBounds(200,200,1024,768);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setBackground(Color.black);
+		window.setLayout(null); //Disabling the default layout.
+
+		Font basicfont = new Font("MS Gothic", Font.PLAIN, 20);
+
+		// Panel Setup
+		JPanel p1 = new JPanel(); //This is where graphics are displayed.
+		p1.setBounds(670, 680, 325, 50); //Panel layout(x,y,x,y)
+		p1.setBackground(Color.black);
+
+		JPanel p2 = new JPanel(); //This is where status is displayed.
+		p2.setBounds(670,10, 325, 350);
+		p2.setBackground(Color.black);
+
+		JPanel p3 = new JPanel(); //This is where story text is displayed.
+		p3.setBounds(10, 370, 650, 350);
+		p3.setBackground(Color.pink);
+
+		JPanel p4 = new JPanel(); //This is where command menu is displayed.
+		p4.setBounds(670, 370, 325, 300);
+		p4.setBackground(Color.black);
+		p4.setLayout(new GridLayout(5,1)); //Setting the layout of Panel 4
+
+		JPanel p5 = new JPanel(); //This is the text-entry box.
+		p5.setBounds(10, 10, 650, 350);
+
+
+
+
+
+
+		// TextArea Setup
+		statusT = new JTextArea();
+		storyT = new JTextArea();
+		input = new JTextField(32);
+
+
+        JButton commandB1 = new JButton("1:Take 1 Stick");
+        JButton commandB2 = new JButton("2:Take 2 Sticks");
+        JButton commandB3 = new JButton("3:Quit");
+		JButton commandB4 = new JButton();
+		JButton commandB5 = new JButton();
+
+		//Graphic setup
+		//graphic = new JButton(new ImageIcon("dungeonSample.jpg"));
+		//graphic.setBackground(Color.black);
+
+
+		//Status text setup
+		statusT.setFont(basicfont);
+		statusT.setBackground(Color.black);
+		statusT.setForeground(Color.white);
+		statusT.setText("hi"); //Ths is just a sample status
+
+		//Story text setup
+		storyT.setFont(basicfont);
+		storyT.setBackground(Color.black);
+		storyT.setForeground(Color.white);
+		storyT.setText(" A pile of sticks has trapped you in the dungeon cell! \n" +
+				    	" You must remove either one or two sticks from the pile in\n" +
+				    	" order to escape. Your goal is to not pick up the last stick.\n" +
+				    	" However, a dragon will join you. Whoever is forced to \n" +
+				    	" to take the last stick will be trapped forever \n\n" +
+						" Type 'start' to begin the dungeon!" );
+
+		//Input box setup
+		input.setFont(basicfont);
+		input.setBackground(Color.black);
+		input.setForeground(Color.white);
+		//input.setText("Input a number here>");
+		input.addActionListener(this);
+
+		// images to icon
+
+        ImageIcon kitchenIcon = new ImageIcon(kitchenPath);
+		ImageIcon kitchenIcon1  = new ImageIcon(kitchenPathKey);
+		ImageIcon dungeonIcon = new ImageIcon(dungeonPath);
+		ImageIcon dungeonIcon2 = new ImageIcon(dungeonPathKey);
+        ImageIcon boilerIcon = new ImageIcon(boilerPath);
+		ImageIcon boilerIcon3 = new ImageIcon(boilerPathKey);
+        ImageIcon libraryIcon = new ImageIcon(libraryPath);
+		ImageIcon libraryIcon4 = new ImageIcon(libraryPathKey);
+        ImageIcon bedroomIcon = new ImageIcon(bedroomPath);
+		ImageIcon bedroomIcon5 = new ImageIcon(bedroomPathKey);
+
+        Image kitchenImage = kitchenIcon.getImage();
+        Image kitchenImage1= kitchenIcon1.getImage();
+		Image dungeonImage = dungeonIcon.getImage();
+		Image dungeonImage2= dungeonIcon2.getImage();
+		Image boilerImage = boilerIcon.getImage();
+		Image boilerImage3 = boilerIcon3.getImage();
+        Image libraryImage = libraryIcon.getImage();
+        Image libraryImage4 = libraryIcon4.getImage();
+        Image bedroomImage = bedroomIcon.getImage();
+        Image bedroomImage5 = bedroomIcon5.getImage();
+
+		Image changedImg = kitchenImage.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg2 = kitchenImage1.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg3 = dungeonImage.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg4 = dungeonImage2.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+        Image changedImg5 = boilerImage.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg6 = boilerImage3.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg7 = libraryImage.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+        Image changedImg8 = libraryImage4.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg9 = bedroomImage.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+		Image changedImg10 = bedroomImage5.getScaledInstance(320, 320, Image.SCALE_SMOOTH );
+
+
+
+	    ImageIcon Icon = new ImageIcon(changedImg);
+	    ImageIcon Icon2 = new ImageIcon(changedImg2);
+		ImageIcon Icon3 = new ImageIcon(changedImg3);
+		ImageIcon Icon4 = new ImageIcon(changedImg4);
+        ImageIcon Icon5 = new ImageIcon(changedImg5);
+	    ImageIcon Icon6 = new ImageIcon(changedImg6);
+		ImageIcon Icon7 = new ImageIcon(changedImg7);
+		ImageIcon Icon8 = new ImageIcon(changedImg8);
+        ImageIcon Icon9 = new ImageIcon(changedImg9);
+	    ImageIcon Icon10 = new ImageIcon(changedImg10);
+
+	 	JButton Image1 = new JButton(Icon);
+		JButton Image2 = new JButton(Icon2);
+		JButton Image3 = new JButton(Icon3);
+		JButton Image4 = new JButton(Icon4);
+        JButton Image5 = new JButton(Icon5);
+		JButton Image6 = new JButton(Icon6);
+        JButton Image7 = new JButton(Icon7);
+		JButton Image8 = new JButton(Icon8);
+        JButton Image9 = new JButton(Icon9);
+		JButton Image10 = new JButton(Icon10);
+
+
+
+    //    Key3 dungeon = new Key3();
+
+    //    boolean dungeonKey = dungeon.getDungeonKey();
+
+
+		// Add Text>>Panel>>Window
+		//p1.add(graphic);
+	    p2.add(Image1);
+		p3.add(storyT);
+		p4.add(commandB1);
+		p4.add(commandB2);
+		p4.add(commandB3);
+		p4.add(commandB4);
+		p4.add(commandB5);
+		p5.add(input);
+
+
+		window.add(p1);
+		window.add(p2);
+		window.add(p3);
+		window.add(p4);
+		window.add(p5);
+
+		window.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+
+		Key3 key3 = new Key3();
+		boolean dragonWin = key3.dragonWin();
+		boolean playerWin = key3.playerWin();
+
+		String choice = input.getText();
+
+			String printStatus = key3.printSticks();
+			storyT.setText(printStatus);
+
+	        if(choice.equalsIgnoreCase("q"))
+	        {
+				key3.totalSticks(1);
+	            storyT.setText("You have taken 1 stick.\n" +
+								"There are " + key3.getTotalSticks() + " sticks left.\n" +
+								"The dragon takes " + key3.dragStick() + " sticks.");
+				key3.totalSticks(key3.dragStick());
+				//dragonWin = key3.dragonWin();
+				//playerWin = key3.playerWin();
+				input.selectAll();
+
+	        }
+
+
+			if(choice.equalsIgnoreCase("w"))
+			{
+				key3.totalSticks(2);
+				storyT.setText("You have taken 2 sticks.\n" +
+								"There are " + key3.getTotalSticks() + " sticks left. \n" +
+								"The dragon takes " + key3.dragStick() + " sticks.");
+				key3.totalSticks(key3.dragStick());
+				//dragonWin = key3.dragonWin();
+				//playerWin = key3.playerWin();
+				input.selectAll();
+			}
+
+			if (dragonWin == true)
+			{
+				storyT.setText("The dragon has won!");
+			}
+			else if(playerWin == true)
+			{
+				storyT.setText("You have won!");
+			}
+
+
+
+	}
+
+}
--- Key3.java
+++ Key3.java
@@ -1,15 +1,14 @@
-//@author Jack Kuang
-
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.Random;
+import javax.swing.JFrame;
+
+
 
 /*
 List of variables
 
 key3 = false: main boolean for the stage loop
-numOfLives = 5: number of lives of the player
-all the other variables are going to be local to this stage
 
 */
 
@@ -17,274 +16,137 @@ all the other variables are going to be local to this stage
 public class Key3
 {
 
-    public boolean mainRoom;
     public boolean key3;
 
-    // instance variables
-    /*public String d_roomTitle;
-    public String d_roomDescription;
+    private boolean dungeonKeyStatus;
+    private static int totalSticks = 21;
+    private int dragTakenSticks;
+    private int playTakenSticks;
 
-    public String Location()
-    {
-    roomTitle = new String("");
-    roomDescription = new String ();
-    }
-
-    public String location (String title)
-    {
-        //Assigns title
-        d_roomTitle = title;
-
-        //blank room description
-        d_roomDescription = new String();
-    }
-
-    // returns location title
-    public String getTitle()
-    {
-        return d_roomTitle;
-    }
-
-    // assigns the location title
-    public void setTitle(String roomTitle)
-    {
-        d_roomTitle = roomTitle;
-    }
-
-    // returns location description
-    public String getDescription()
-    {
-        return d_roomDescription;
-    }
-
-    // assigns location description
-    public void setDescription(String roomDescription)
-    {
-        d_roomDescription = roomDescription;
-    }
-*/
 
 //------------------------------------------------------------------------------
-void dungeonStart()
-{
-    boolean key3 = false;
-    Scanner userInput = new Scanner(System.in);
+// a list of most of the print information for the class
 
-    // main description of this stage and the possible options
-/*    String description  = "In level 3 there are 2 levels as well. On the right is a certain level \n"
-    + "and on the left is another level, both of which you can enter and leave at will.\n"
-    + "You cannot proceed to the next part of the game without first solving this stage first. \n";
 
-    System.out.println("\nWelcome to level 3! Another stage consisting of two parts!\n");
+    String dungeonEntranceInfo = "\nYou have entered the dungeon. It is dark all around. You see a\n"
+    + "spark. Flames blazing! There is a dragon! You must solve the puzzle to escape! \n";
+    String winGame = "\nYou have outsmarted the dragon. You are free to leave!";
+    String loseGame = "You are forced to take the last stick. You are trapped for eternity!";
+    String displayStickAmount = "\nThere are " + totalSticks + " sticks in the pile";
+    //String displayPlayerAmount = "\nYou have taken " + takenSticks + " sticks.";
+    //String displayDragonAmount = "\nThe dragon takes " + takenSticks + " sticks.";
+    String stickError = "\nYou can only take one or two sticks!\n";
 
+
+//------------------------------------------------------------------------------
+/**
+* Method that gets the total stick amount
+*
+* parameters: none
+*
+* retuns: integer
 */
-        // main game loop that repeats until the player is out of lives
-        if (key3 == false)
-        {
-            //System.out.println(Description);
-            System.out.println("Would you like to go left (L) into the dungeon? ");
-
-                // pop up box prompting user if they want to go left or right
-                Object[] possibleValues = { "Left", "Go back" };
-                String leftOrRight = (String)JOptionPane.showInputDialog(null,
-                                "What is your move?", "Input",
-                                JOptionPane.INFORMATION_MESSAGE, null,
-                                possibleValues, possibleValues[0]);
-
-
-            // variable to decide if the player is still in the main game loop or exits
-            boolean mainRoom = false;
-
-                // checks if the player wants to play the game and starts/exits accordingly
-                if (leftOrRight.equalsIgnoreCase("Left") && (mainRoom == false))
-                {
-                    System.out.println("\nYou have entered the dungeon. It is dark all around. You see a\n"
-                    + "spark. Flames blazing! There is a dragon! You must solve the puzzle to escape!");
-                //    System.out.println("Player options: \n(P) to attempt the puzzle\n(E) to escape\n(Q) to quit game");
-                //    System.out.print("Choice: ");
-
-                } else
-                {
-                    System.out.println("You have gone back to the main room!");
-                }
-            }
-
+public int getTotalSticks()
+{
+    return (totalSticks);
 }
 
 
 //------------------------------------------------------------------------------
-void colorGame()
+/**
+* method to calculate if the dragon/computer has taken the last stick. If it did,
+* it lost the game. If it didn't, the computer has won.
+*
+* parameters; none
+*
+* returns: boolean
+*/
+boolean dragonWin()
 {
-    Object[] possibleDungeonValues = { "Puzzle", "Escape", "Quit" };
-    String dungeonChoice = (String)JOptionPane.showInputDialog(null,
-                    "What is your move?", "Input",
-                    JOptionPane.QUESTION_MESSAGE, null,
-                    possibleDungeonValues, possibleDungeonValues[0]);
-
-                    // begins the puzzle
-                    if (dungeonChoice.equalsIgnoreCase("Puzzle"))
-                    {
-                        System.out.println("The puzzle has commenced. Are you ready to fight for your life?");
-
-                        int numOfLives = 5;
-                        boolean colorStage = false;
-                        int randomColor = new Random().nextInt(10);
-                        Scanner color = new Scanner(System.in);
-
-                        System.out.println("Answer correctly what each combination of color makes!");
-
-                        // color minigame loop that runs until the player is out of lives
-                        // or the player decides to exit
-                        while (colorStage == false)
-                        {
-                            System.out.println("Number of lives: " + numOfLives);
-
-                            if (randomColor == 1) // YELLOW AND RED
-                            {
-                                System.out.print("\nYellow and red makes? ");
-                                String colorChoice1 = color.nextLine();
-                                if (colorChoice1.equalsIgnoreCase("orange"))
-                                {
-                                    System.out.print("Correct!");
-                                } else
-                                {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }
-                            }
-                            else if (randomColor == 2) { // RED AND BLUE
-                                System.out.print("\nRed and blue makes? ");
-                                String colorChoice2 = color.nextLine();
-                                if (colorChoice2.equalsIgnoreCase("purple")) {
-                                    System.out.print("Correct!");
-                                } else {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                } }
-                            else if (randomColor == 3) { //BLUE AND YELLOW
-                                System.out.print("\nBlue and yellow makes? ");
-                                String colorChoice3 = color.nextLine();
-                                if (colorChoice3.equalsIgnoreCase("green")) {
-                                    System.out.print("Correct!");
-                                } else {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                } }
-                            else if (randomColor == 4) { // RED AND GREEN
-                                System.out.print("\nRed and green makes? ");
-                                String colorChoice4 = color.nextLine();
-                                if (colorChoice4.equalsIgnoreCase("brown")) {
-                                    System.out.println("Correct!");
-                                } else {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }}
-                            else if (randomColor == 5) { // YELLOW AND RED
-                                System.out.print("\nYellow and red makes? ");
-                                String colorChoice5 = color.nextLine();
-                                if (colorChoice5.equalsIgnoreCase("yellow green") || colorChoice5.equalsIgnoreCase("green yellow")) {
-                                    System.out.println("Correct!");
-                                } else {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }}
-                            else if (randomColor == 6) { // BLUE AND GREEN
-                                System.out.print("\nBlue and Green makes? ");
-                                String colorChoice6 = color.nextLine();
-                                if (colorChoice6.equalsIgnoreCase("aquamarine")) {
-                                    System.out.print("Correct!");
-                                } else {
-                                    System.out.println("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }}
-                            else if (randomColor == 7) { // RED AND WHITE
-                                System.out.print("\nRed and white makes? ");
-                                String colorChoice7 = color.nextLine();
-                                if (colorChoice7.equalsIgnoreCase("pink")) {
-                                    System.out.println("Correct!");
-                                } else {
-                                    System.out.print("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }}
-                            else if (randomColor == 8) { // BLACK AND WHITE
-                                System.out.print("\nBlack and white makes? ");
-                                String colorChoice8 = color.nextLine();
-                                if (colorChoice8.equalsIgnoreCase("grey") || colorChoice8.equalsIgnoreCase("gray")) {
-                                    System.out.println("Correct!");
-                                } else {
-                                    System.out.print("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }}
-                            else if (randomColor == 9) // WHITE AND PURPLE
-                            {
-                                System.out.print("\nWhite and purple makes? ");
-                                String colorChoice9 = color.nextLine();
-                                if (colorChoice9.equalsIgnoreCase("light purple")) {
-                                    System.out.println("Correct!");
-                                } else
-                                {
-                                    System.out.print("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }
-                            }
-                            else if (randomColor == 10) // RED AND BLACK
-                            {
-                                System.out.print("\nRed and black makes? ");
-                                String colorChoice10 = color.nextLine();
-                                if (colorChoice10.equalsIgnoreCase("pink")) {
-                                    System.out.println("Correct!");
-                                } else
-                                {
-                                    System.out.print("Incorrect! You have lost a life!");
-                                    numOfLives = numOfLives - 1;
-                                }
-                            }
-                        }
-                        // shows player's number of lives and promtpts if the player wants to play again
-                        System.out.println("\nNumber of lives: " + numOfLives);
-                        System.out.println();
-                        System.out.print("Would you like to play again [(Y) or (N)]? ");
-                        String playAgain = color.nextLine();
-                        // replays the color game if the player enters y
-                        if (playAgain.equalsIgnoreCase("y"))
-                        {
-                            colorStage = false;
-                            randomColor = randomColor + 1;
-                            if (randomColor == 10)
-                            {
-                                randomColor = 1;
-                            }
-                        // program automatically ends game if lives reach 0
-                        } else if(numOfLives == 0)
-                        {
-                            System.out.println("You have died. Rest in peace!");
-                            key3 = true;
-                        }
-                        // program exits back to main loop if player no longer wants to play color game
-                        else if (playAgain.equalsIgnoreCase("n"))
-                        {
-                            System.out.println("You have gone back to the main room");
-                            colorStage = true;
-                        } else
-                        {
-                            System.out.print("That is not a valid input.");
-                        }
+    boolean dragonVictory = false;
+    if(totalSticks <=0)
+    {
+    dragonVictory = true;
+    }
+    return (dragonVictory);
+}
 
 
-                    }
-                    else if (dungeonChoice.equalsIgnoreCase("Escape"))
-                    {
-                        System.out.println("\nYou run for your life! You have returned to the main room.\n");
-                        mainRoom = true;
-                    } else if (dungeonChoice.equalsIgnoreCase("Quit"))
-                    {
-                        System.out.println("Aww, sad to see you go. Try again soon!");
-                        key3 = true;
-                    }
+//------------------------------------------------------------------------------
+/**
+* method to calculate if the player has taken the last stick. If they did,
+* they lost the game. If they didn't, the have won.
+*
+* parameters; none
+*
+* returns: boolean
+*/
+boolean playerWin()
+{
+    boolean playerVictory = false;
+    if (totalSticks <= 0 )
+    {
+        playerVictory = true;
+    }
+    return(playerVictory);
+}
 
-            else
-            {
-                System.out.println("That is an invalid input. Please select (R) or (L)!");
-            }
-        }
+
+
+//------------------------------------------------------------------------------
+/**
+* method to calculate the total amount of sticks left by subtracting the amount
+* the player and dragon takes
+*
+* parameters; integer
+*
+* returns: none
+*/
+void totalSticks(int takenSticks)
+{
+    totalSticks = totalSticks - takenSticks;
+}
+
+//------------------------------------------------------------------------------
+/**
+* method to that prints the amount of sticks left in the pile and promts the user
+* for how many sticks they would like to pick up in the next turn
+*
+* parameters; none
+*
+* returns: integer
+*/
+String printSticks()
+{
+    String stickLeft = "There are " + totalSticks + " sticks left.\n" +
+                        "Type q to pick up 1 stick \n" +
+                        "Type w to pick up 2 sticks";
+    return (stickLeft);
+}
+
+//------------------------------------------------------------------------------
+/**
+* method that calculates how many sticks the dragon (computer) will pick up
+* The amount taken is a simple math algorithm so that the computer will try not
+* to pick up the last stick. It will then return the amount of sticks taken
+*
+* parameters; none
+*
+* returns: integer
+*/
+int dragStick()
+{
+    if (totalSticks - 2 == 0 || (totalSticks - 2) % 3 == 0)
+    {
+        dragTakenSticks = 1;
+    }
+    else
+    {
+        dragTakenSticks = 2;
+    }
+
+    return(dragTakenSticks);
+
+}
+
 }
