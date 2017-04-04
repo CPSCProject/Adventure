64d8cd2c20cc16ae3341da024203598a8e81b428
erikafung <erikaef978@gmail.com>
2017-04-04 17:45:03 -0600

    APR 4 FILE IO AND EXCEPTION HANDLING

--- Library.java
+++ Library.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.*;
+
 /**
 *The Library program runs the logic of the GUI Main.java
 *this class represents a player of the game going through 8 riddles
@@ -11,9 +21,14 @@ public class Library
 {
   //initialize keyboard and public variables gameOn (controls if level runs) and lives (differ depending on difficulty)
   Scanner keyboard = new Scanner(System.in);
-  public boolean gameOn;
+  //public boolean gameOn;
   //public int lives=5;
-  public boolean key1;
+  public static boolean key1;
+  List<String> name = new ArrayList<String>();
+  String[] namesArray;
+  String line;
+  BufferedReader br = null;
+  FileReader fr = null;
 
 //---------------------------------------------------------------------------------------------------------
 
@@ -32,8 +47,22 @@ public String puzzle1method(String parameter){
   String puzzle1 = "error";
   if (parameter.equals("mailbox"))
     {
-      puzzle1 = "Correct! \nRiddle 2: What is it that after you take away the whole, some still remains? \n1. wholesome \n2. donut";//answer is correct
-    }
+      try {
+        fr = new FileReader("Riddles.txt");
+        br = new BufferedReader(fr);
+        br = new BufferedReader(new FileReader("Riddles.txt"));
+        while ((line = br.readLine()) != null) {
+          name.add(line);
+        }
+        namesArray = name.toArray(new String[name.size()]);
+        puzzle1 = name.get(0);
+		}
+    catch(IOException e){
+          System.out.println("Error opening the file Riddles.txt");
+          //System.exit(0);
+      }
+      //puzzle1 = "Correct! \nRiddle 2: What is it that after you take away the whole, \nsome still remains? \n1. wholesome \n2. donut";//answer is correct
+  }
     else
     {
       puzzle1 = "Incorrect. ";
@@ -51,8 +80,27 @@ public String puzzle1method(String parameter){
     String puzzle2 = "error";
     if (parameter.equals("wholesome"))
       {
-        puzzle2 = "Correct! \nRiddle 3: Name an eight letter word that has kst in the middle, \nin the beginning, and at the end. \n1. instand \n2. kstkstkst";//answer is correct
+        try {
+          fr = new FileReader("Riddles.txt");
+          br = new BufferedReader(fr);
+          br = new BufferedReader(new FileReader("Riddles.txt"));
+          while ((line = br.readLine()) != null) {
+            name.add(line);
+          }
+          namesArray = name.toArray(new String[name.size()]);
+          puzzle2 = name.get(1);
+        }
+      catch (IndexOutOfBoundsException e) {
+        puzzle2 = "Index is out of bounds";
       }
+      catch (FileNotFoundException e) {
+        puzzle2 = "File is not found";
+      }
+      catch (IOException e) {
+        puzzle2 = "IOexception arose";
+      }
+    }
+        //puzzle2 = "Correct! \nRiddle 3: Name an eight letter word that has kst in the middle, \nin the beginning, and at the end. \n1. instand \n2. kstkstkst";//answer is correct
       else
       {
         puzzle2 = "Incorrect. ";
@@ -70,7 +118,26 @@ public String puzzle1method(String parameter){
     String puzzle3 = "error";
     if (parameter.equals("inkstand"))
       {
-        puzzle3 = "Correct! \nUnscramble 1: Arrange these letters into a word: \nN D U E D R E F  \n1. refunded \n2. deafened";//answer is correct
+        try {
+          fr = new FileReader("Riddles.txt");
+          br = new BufferedReader(fr);
+          br = new BufferedReader(new FileReader("Riddles.txt"));
+          while ((line = br.readLine()) != null) {
+            name.add(line);
+          }
+          namesArray = name.toArray(new String[name.size()]);
+          puzzle3 = name.get(2);
+        }
+      catch (IndexOutOfBoundsException e) {
+        puzzle3 = "Index is out of bounds";
+      }
+      catch (FileNotFoundException e) {
+        puzzle3 = "File is not found";
+      }
+      catch (IOException e) {
+        puzzle3 = "IOexception arose";
+      }
+        //puzzle3 = "Correct! \nUnscramble 1: Arrange these letters into a word: \nN D U E D R E F  \n1. refunded \n2. deafened";//answer is correct
       }
       else
       {
@@ -89,7 +156,26 @@ public String puzzle1method(String parameter){
   String puzzle4 = "error";
   if (parameter.equals("refunded"))
    {
-    puzzle4 = "Correct! \nUnscramble 2: Arrange these letters into a word: \nV R A E S N T O B \n1. observant \n2. observance";
+     try {
+       fr = new FileReader("Riddles.txt");
+       br = new BufferedReader(fr);
+       br = new BufferedReader(new FileReader("Riddles.txt"));
+       while ((line = br.readLine()) != null) {
+         name.add(line);
+       }
+       namesArray = name.toArray(new String[name.size()]);
+       puzzle4 = name.get(3);
+     }
+   catch (IndexOutOfBoundsException e) {
+     puzzle4 = "Index is out of bounds";
+   }
+   catch (FileNotFoundException e) {
+     puzzle4 = "File is not found";
+   }
+   catch (IOException e) {
+     puzzle4 = "IOexception arose";
+   }
+    //puzzle4 = "Correct! \nUnscramble 2: Arrange these letters into a word: \nV R A E S N T O B \n1. observant \n2. observance";
    }
   else
    {
@@ -108,7 +194,26 @@ public String puzzle1method(String parameter){
     String puzzle5 = "error";
     if (parameter.equals("observant"))
     {
-      puzzle5 = "Correct! \nUnscramble 3: Arrange these letters into a word: \nS F N L O I G M A \n1. flamingos \n2. flamenco";//answer is correct
+      try {
+        fr = new FileReader("Riddles.txt");
+        br = new BufferedReader(fr);
+        br = new BufferedReader(new FileReader("Riddles.txt"));
+        while ((line = br.readLine()) != null) {
+          name.add(line);
+        }
+        namesArray = name.toArray(new String[name.size()]);
+        puzzle5 = name.get(4);
+      }
+    catch (IndexOutOfBoundsException e) {
+      puzzle5 = "Index is out of bounds";
+    }
+    catch (FileNotFoundException e) {
+      puzzle5 = "File is not found";
+    }
+    catch (IOException e) {
+      puzzle5 = "IOexception arose";
+    }
+      //puzzle5 = "Correct! \nUnscramble 3: Arrange these letters into a word: \nS F N L O I G M A \n1. flamingos \n2. flamenco";//answer is correct
     }
     else
     {
@@ -127,7 +232,26 @@ public String puzzle1method(String parameter){
     String puzzle6 = "error";
     if (parameter.equals("flamingos"))
       {
-        puzzle6 = "Correct! \nCombination 1: Find the correct combination of these numbers \n 36 27 11 \n1. 361127 \n2. 112736";//answer is correct
+        try {
+          fr = new FileReader("Riddles.txt");
+          br = new BufferedReader(fr);
+          br = new BufferedReader(new FileReader("Riddles.txt"));
+          while ((line = br.readLine()) != null) {
+            name.add(line);
+          }
+          namesArray = name.toArray(new String[name.size()]);
+          puzzle6 = name.get(5);
+        }
+      catch (IndexOutOfBoundsException e) {
+        puzzle6 = "Index is out of bounds";
+      }
+      catch (FileNotFoundException e) {
+        puzzle6 = "File is not found";
+      }
+      catch (IOException e) {
+        puzzle6 = "IOexception arose";
+      }
+        //puzzle6 = "Correct! \nCombination 1: Find the correct combination of these numbers \n 36 27 11 \n1. 361127 \n2. 112736";//answer is correct
       }
     else
       {
@@ -146,7 +270,26 @@ public String puzzle1method(String parameter){
     String puzzle7 = "error";
     if (parameter.equals("112736"))
       {
-        puzzle7 = "Correct! \nCombination 2: Find the correct combination of these numbers \n 42 19 04 \n1. 190442 \n2. 041942";//answer is correct
+        try {
+          fr = new FileReader("Riddles.txt");
+          br = new BufferedReader(fr);
+          br = new BufferedReader(new FileReader("Riddles.txt"));
+          while ((line = br.readLine()) != null) {
+            name.add(line);
+          }
+          namesArray = name.toArray(new String[name.size()]);
+          puzzle7 = name.get(6);
+        }
+      catch (IndexOutOfBoundsException e) {
+        puzzle7 = "Index is out of bounds";
+      }
+      catch (FileNotFoundException e) {
+        puzzle7 = "File is not found";
+      }
+      catch (IOException e) {
+        puzzle7 = "IOexception arose";
+      }
+        //puzzle7 = "Correct! \nCombination 2: Find the correct combination of these numbers \n 42 19 04 \n1. 190442 \n2. 041942";//answer is correct
       }
     else
       {
@@ -165,7 +308,27 @@ public String puzzle1method(String parameter){
     String puzzle8 = "error";
     if (parameter.equals("190442"))
       {
-        puzzle8 = "Correct! \n The level has been completed! ";//answer is correct
+        try {
+          fr = new FileReader("Riddles.txt");
+          br = new BufferedReader(fr);
+          br = new BufferedReader(new FileReader("Riddles.txt"));
+          while ((line = br.readLine()) != null) {
+            name.add(line);
+          }
+          namesArray = name.toArray(new String[name.size()]);
+          puzzle8 = name.get(7);
+        }
+      catch (IndexOutOfBoundsException e) {
+        puzzle8 = "Index is out of bounds";
+      }
+      catch (FileNotFoundException e) {
+        puzzle8 = "File is not found";
+      }
+      catch (IOException e) {
+        puzzle8 = "IOexception arose";
+      }
+        //String puzzle8raw = "Correct! \n The level has been completed! \n"; //+ str;
+        //puzzle8 = puzzle8raw;//answer is correct
       }
     else
       {
@@ -174,4 +337,11 @@ public String puzzle1method(String parameter){
       String puzzle8return = puzzle8;
       return puzzle8return;
     }
+/*
+  public boolean key1Control() {
+    if (!puzzle1.equals("error") && !puzzle2.equals("error") && !puzzle3.equals("error") && !puzzle4.equals("error") && !puzzle5.equals("error") && !puzzle6.equals("error") && !puzzle7.equals("error") && !puzzle8.equals("error")){
+      key1 = true;
+    }
+  }
+  */
 }
--- Main.java
+++ Main.java
@@ -1,21 +1,13 @@
-import GUI.TimerR;
-import rooms.Library;
-import rooms.Kitchen;
-import rooms.Boiler;
-import rooms.Key3;
-
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-/*
-*The Main class inherits the Jframe and implements the ActionListener.
-*The Main class creates a JFrame with 4 panels.
-*Instances are made in this class to call methods in classes Kitchen and Library.
-*Input from user is taken as a parameter to these methods
-*corresponding output is the returned from the methods in Class Kitchen and Class Library.
-*The output is then displayed in the TextArea with a following menu.
+
+/**
+*GUI panel that displays the logic of the Library class with 8 riddles
+*Each action creates an instance of Library class and runs one method
+*code adapted from http://stackoverflow.com/questions/29578570/having-a-trouble-in-making-a-text-based-rpg-text-area-field-related
 */
+
 public class Main extends JFrame implements ActionListener
 {
 JFrame window;
@@ -30,111 +22,125 @@ JTextArea storyT;
 JTextField input;
 JButton commandB1;
 JButton commandB2;
-JButton Image1;
+JButton commandB3;
+JButton commandB4;
+JButton commandB5;
 Timer timer;
 
 public static void main(String[]args)
 {
+    String display1 = "\n   Riddle 1: \n   What is a 7 letter word containing thousands of letters?   \n";
+
+    //String display2 = "Riddle 2: \nWhat is it that after you take away the whole, some still remains?";
+
+    //String display3 = "Riddle 3: \nName an eight letter word that has kst in the middle, in the beginning, and at the end. ";
 
     Main game = new Main();
-    game.opening();
+    game.opening(display1);
 
 }
 
-
-
-public void opening()
+public void opening(String displaymessage)
 {
 
+
     JFrame window = new JFrame();
-    window.setBounds(200,50,1024,768);
+    window.setBounds(200,200,1024,868);
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setBackground(Color.black);
-    window.setLayout(null); //Disabling the default layout.
+    window.setBackground(Color.pink);
+    window.setLayout(new GridLayout(4,2)); //Disabling the default layout.
 
-    Font basicfont = new Font("MS Gothic", Font.PLAIN, 20);
-    // Panel Setup
+    Font basicfont = new Font("Comic Sans", Font.PLAIN, 20);
+
+  // Panel Setup
     JPanel p1 = new JPanel(); //This is where graphics are displayed.
-    p1.setBounds(670, 680, 325, 50); //Panel layout(x,y,x,y)
+    p1.setBounds(670, 675, 325, 140); //Panel layout(x,y,x,y)
     p1.setBackground(Color.pink);
-
 
     JPanel p2 = new JPanel(); //This is where status is displayed.
     p2.setBounds(670, 10, 325, 350);
-    p2.setBackground(Color.black);
+    p2.setBackground(Color.pink);
 
     JPanel p3 = new JPanel(); //This is where story text is displayed.
-    p3.setBounds(10, 370, 650, 350);
-    p3.setBackground(Color.black);
+    p3.setBounds(0, 210, 660, 550);
+    p3.setBackground(Color.pink);
 
     JPanel p4 = new JPanel(); //This is where command menu is displayed.
     p4.setBounds(670, 370, 325, 300);
-    p4.setBackground(Color.black);
+    p4.setBackground(Color.pink);
     p4.setLayout(new GridLayout(5,1)); //Setting the layout of Panel 4
 
     JPanel p5 = new JPanel(); //This is the text-entry box.
-    p5.setBounds(60, 60, 650, 350);
+    p5.setBounds(10, 80, 650, 350);
 
-    TimerR panel = new TimerR();//This is the Timer panel
-    panel.setBounds(20,20,250,250);
 
-//------------------------------------------------------------------------------
     // TextArea Setup
     statusT = new JTextArea();
     storyT = new JTextArea();
     input = new JTextField(32);
 
 
-    JButton commandB1 = new JButton("Go to previous menu");
-    JButton commandB2 = new JButton("Quit");
-
-
-
-
-
+    JButton commandB1 = new JButton("1: Quit game");
+    commandB1.setBackground(Color.pink);
+    commandB1.setForeground(Color.white);
+    JButton commandB2 = new JButton("2: view difficulty");
+    commandB2.setBackground(Color.white);
+    commandB2.setForeground(Color.pink);
+    /*
+    JButton commandB3 = new JButton("");
+    commandB3.setBackground(Color.pink);
+    commandB3.setForeground(Color.white);
+    JButton commandB4 = new JButton("");
+    commandB4.setBackground(Color.white);
+    commandB4.setForeground(Color.pink);
+    JButton commandB5 = new JButton("");
+    commandB5.setBackground(Color.pink);
+    commandB5.setForeground(Color.white);
+    */
     //Graphic setup
-    //graphic = new JButton(new ImageIcon("dungeonSample.jpg"));
-    //graphic.setBackground(Color.black);
+    //graphic = new JButton(new ImageIcon("flower.jpg"));
+    //graphic.setBackground(Color.pink);
 
 
     //Status text setup
     statusT.setFont(basicfont);
-    statusT.setBackground(Color.black);
-    statusT.setForeground(Color.white);
-
+    statusT.setBackground(Color.pink);
+    statusT.setForeground(Color.black);
+    //statusT.setText("\n\n   LV     Kitchen   \n\n   HP   100   \n\n   MP    28   \n\n   EXP   36   \n\n"); //Ths is just a sample status
 
     //Story text setup
     storyT.setFont(basicfont);
-    storyT.setBackground(Color.black);
-    storyT.setForeground(Color.white);
-    storyT.setText("Adventure Game: input 'start' to start game");
+    storyT.setBackground(Color.pink);
+    storyT.setForeground(Color.black);
+    storyT.setText(displaymessage);
 
     //Input box setup
     input.setFont(basicfont);
-    input.setBackground(Color.black);
-    input.setForeground(Color.white);
-    //input.setText("Input a number here>");
+    input.setBackground(Color.white);
+    input.setForeground(Color.black);
+    //input.setText("Answer: ");
     input.addActionListener(this);
-
-    // add map jpg to top right panel
-    String mapPath = "map.jpg";
-    ImageIcon mapIcon = new ImageIcon(mapPath);
-    Image mapImage = mapIcon.getImage();
-    Image changedImg = mapImage.getScaledInstance(120,120, Image.SCALE_SMOOTH);
-    ImageIcon Icon = new ImageIcon(changedImg);
-    JButton Image1 = new JButton(Icon);
 
 
 
     // Add Text>>Panel>>Window
-    //p1.add(graphic);
-    p2.add(Image1);
-    p2.add(statusT);
-    p3.add(storyT);
+
+    p3.add(statusT);
+    p2.add(storyT);
     p4.add(commandB1);
-    commandB1.addActionListener(new Action());
     p4.add(commandB2);
+
+    commandB1.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent arg0) {
+      JOptionPane.showMessageDialog(null, "You have quit the game. ");
+            }
+    });
+    //p4.add(commandB3);
+    //p4.add(commandB4);
+    //p4.add(commandB5);
     p5.add(input);
+    //p1.add(graphic);
+
 
     window.add(p1);
     window.add(p2);
@@ -142,20 +148,14 @@ public void opening()
     window.add(p4);
     window.add(p5);
 
-
-    window.add(panel);//Add the timer to the JFrame
-    window.setLocationByPlatform( true );
     window.setVisible(true);
-
-    //The main menu is shown before any input.
-    storyT.setText("You have entered the library.\nRiddle 1:\nWhat is a 7 letter\nword containing thousands of letters? \n1. mailbox \n2. alphabet ");
-
+    storyT.setText("You have entered the library. \nRiddle 1: What is a 7 letter word containing thousands of letters? \n1. mailbox \n2. alphabet ");
 }
+
 
 public void actionPerformed(ActionEvent e)
 {
-//--------------------------LIBRARY------------------------------------------------
-String lchoice = input.getText();
+    String lchoice = input.getText();
     if (lchoice.equals("mailbox") || lchoice.equals("alphabet")) {
       Library key1p1 = new Library();
       String puzzle1menu = key1p1.puzzle1method(lchoice);
@@ -210,60 +210,7 @@ String lchoice = input.getText();
       String puzzle8menu = key1p8.puzzle8method(lchoice8);
       storyT.setText(puzzle8menu);
       input.selectAll();
-
-    }
-//--------------------------KITCHEN--------------------------------------------------
-
-
-        String choice = input.getText();
-        if(choice.equals("t") || choice.equals("c")){
-          Kitchen key2 = new Kitchen();
-          String table_menu = key2.KitchenExploreTable(choice);
-          storyT.setText(table_menu);
-          System.out.println("choice is:" + choice);
-          //input.setText(null);
-          input.selectAll();
-          //Boolean table = false;
-        }
-        //Displays menu for the table. Player can pick paper or knife to store in the inventory.
-          String choice2 = choice;
-          Kitchen key2_paper = new Kitchen();
-          if(choice2.equals("k")|| choice2.equals("p")){ //if statement to prevent it from skipping menus.
-          String paper_menu = key2_paper.KitchenExplorePaper(choice2);
-          storyT.setText(paper_menu);
-          System.out.println("choice2 is:" + choice2);
-          //input.setText(null);
-          input.selectAll();
-        }
-
-        //Displays menu for the cabinet. Player can try to unlock the.
-        String choice3 = choice2;
-        Kitchen key2_cabinet = new Kitchen();
-        if(choice3.equals("e")|| choice3.equals("l")){
-        String cabinet_menu = key2_cabinet.KitchenExploreCabinet(choice3);
-        storyT.setText(cabinet_menu);
-        System.out.println("choice3 is:" + choice3);
-        //input.setText(null);
-        input.selectAll();
-      }
-
-      String choice4 = choice3;
-      Kitchen key2_code = new Kitchen();
-      if(choice4.equals("345")|| choice4.equals("987")){
-      String code_menu = key2_code.KitchenExploreCode(choice4);
-      storyT.setText(code_menu);
-      System.out.println("choice4 is:" + choice3);
-      //input.setText(null);
-      input.selectAll();
     }
 
-
-
-}
-//Action listerner for quit button.
-public class Action implements ActionListener{
-public void actionPerformed(ActionEvent b){
- storyT.setText("Do you wanna quit?");
-}
 }
 }
