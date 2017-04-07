6029771a73ae3cdaa103e919512f52697ccbd2e9
erikafung <erikaef978@gmail.com>
2017-04-07 10:48:22 -0600

    APRIL 7 IT WORKS WITH ALL THE SHIT

--- Graphics.java
+++ Graphics.java
@@ -1,19 +1,10 @@
-/*
-Hey guys, this is the version of the GUI for the game. Most of the main stuff is
-layed out.
-A few more things need to be added, like the timer and the maps. If these can be
-added, it would make it a lot easier to start adding in menu options and determining
-card order.
-When the program starts, it comes as a really small pop up instead of a full sized
-window. I'm not sure how to fix this feel free to fix it if you know the problem.
-*/
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Graphics extends JFrame
+public class Graphics extends JFrame implements ActionListener
 
 {
 
@@ -27,11 +18,11 @@ JPanel mappanel; //02
 JPanel answerpanel; //11
 JPanel buttonpanel; //12
 JPanel timerpanel; //20
-
+JTextArea storyT;
 JButton quitbutton;
 JButton getclue;
-
 JTextField input;
+
 
 
 public void opening()
@@ -70,7 +61,10 @@ public void opening()
   c.weightx = 10.0;
   c.weighty = 15.0;
   c.fill = GridBagConstraints.BOTH;
-  questionpanel.add(new JTextArea("This is where the question will go."));
+  //questionpanel.add(new JTextArea("This is where the question will go."));
+  storyT = new JTextArea("You have entered the library. \nRiddle 1: What is a 7 letter word containing thousands of letters? \n1. mailbox \n2. alphabet ");
+  questionpanel.add(storyT);
+  //question1panel.add(new JTextArea("You have entered the library. \nRiddle 1: What is a 7 letter word containing thousands of letters? \n1. mailbox \n2. alphabet "));
   questionpanel.setBackground(Color.PINK);
   mainPanel.add(questionpanel,c);
 
@@ -109,19 +103,15 @@ public void opening()
   dungeon.addActionListener(this);
   dungeon.setActionCommand("Dungeon");
   dungeon.addActionListener(new ButtonListener());
-
   kitchen.addActionListener(this);
   kitchen.setActionCommand("Kitchen");
   kitchen.addActionListener(new ButtonListener());
-
   boiler.addActionListener(this);
   boiler.setActionCommand("Boiler");
   boiler.addActionListener(new ButtonListener());
-
   library.addActionListener(this);
   library.setActionCommand("Library");
   library.addActionListener(new ButtonListener());
-
   bedroom.addActionListener(this);
   bedroom.setActionCommand("Bedroom");
   bedroom.addActionListener(new ButtonListener()); */
@@ -169,18 +159,22 @@ public void opening()
 
 //written style card
   JPanel writtenresponse = new JPanel();
+  //JTextField input;
   writtenresponse.add(new JLabel("Enter your answer:"));
-  writtenresponse.add(new JTextField(50));
+  input = new JTextField(50);
+  writtenresponse.add(input);
 //panel position in GridBagLayout
   c.gridx = 1;
   c.gridy = 1;
   c.weightx = 10.0;
   c.weighty = 10.0;
   c.fill = GridBagConstraints.BOTH;
-  answerpanel.add(mcq);
+  //answerpanel.add(mcq);
   answerpanel.add(writtenresponse);
   answerpanel.setBackground(Color.GREEN);
+  input.addActionListener(this);
   mainPanel.add(answerpanel,c);
+
 
 //Add button panel--------------------------------------------------------------
   JPanel buttonpanel = new JPanel();
@@ -211,7 +205,69 @@ public void opening()
   mainframe.pack();
   mainframe.setLocationByPlatform(true);
   mainframe.setVisible(true);
+
   }
+
+
+  public void actionPerformed(ActionEvent e)
+  {
+      String lchoice = input.getText();
+      if (lchoice.equals("mailbox") || lchoice.equals("alphabet")) {
+        Library key1p1 = new Library();
+        String puzzle1menu = key1p1.puzzle1method(lchoice);
+        storyT.setText(puzzle1menu);
+        input.selectAll();
+      }
+      String lchoice2 = input.getText();
+      if (lchoice2.equals("wholesome") || lchoice2.equals("donut")) {
+        Library key1p2 = new Library();
+        String puzzle2menu = key1p2.puzzle2method(lchoice2);
+        storyT.setText(puzzle2menu);
+        input.selectAll();
+      }
+      String lchoice3 = input.getText();
+      if (lchoice3.equals("inkstand") || lchoice3.equals("kstkstkst")) {
+        Library key1p3 = new Library();
+        String puzzle3menu = key1p3.puzzle3method(lchoice3);
+        storyT.setText(puzzle3menu);
+        input.selectAll();
+      }
+      String lchoice4 = input.getText();
+      if (lchoice4.equals("refunded") || lchoice4.equals("deafened")) {
+        Library key1p4 = new Library();
+        String puzzle4menu = key1p4.puzzle4method(lchoice4);
+        storyT.setText(puzzle4menu);
+        input.selectAll();
+      }
+      String lchoice5 = input.getText();
+      if (lchoice5.equals("observant") || lchoice5.equals("observance")) {
+        Library key1p5 = new Library();
+        String puzzle5menu = key1p5.puzzle5method(lchoice5);
+        storyT.setText(puzzle5menu);
+        input.selectAll();
+      }
+      String lchoice6 = input.getText();
+      if (lchoice6.equals("flamingos") || lchoice6.equals("flamenco")) {
+        Library key1p6 = new Library();
+        String puzzle6menu = key1p6.puzzle6method(lchoice6);
+        storyT.setText(puzzle6menu);
+        input.selectAll();
+      }
+      String lchoice7 = input.getText();
+      if (lchoice7.equals("361127") || lchoice7.equals("112736")) {
+        Library key1p7 = new Library();
+        String puzzle7menu = key1p7.puzzle7method(lchoice7);
+        storyT.setText(puzzle7menu);
+        input.selectAll();
+      }
+      String lchoice8 = input.getText();
+      if (lchoice8.equals("190442") || lchoice8.equals("041942")) {
+        Library key1p8 = new Library();
+        String puzzle8menu = key1p8.puzzle8method(lchoice8);
+        storyT.setText(puzzle8menu);
+        input.selectAll();
+      }
+}
 
   public static void main(String[]args)
   {
@@ -224,43 +280,30 @@ public void opening()
   {
           ButtonListener() {
           }
-
-
     /*      /// to make it perform
           public void actionPerformed(ActionEvent eve)
           {
-
-
               if(eve.getActionCommand() == "1:Help") {
                   JOptionPane.showMessageDialog(null, "Hello");
-
                   //commandB1.setVisible(false);
                   //JButton Image1 = new JButton(Icon5);
-
               }
-
               // when you first pressed one button
               else if(eve.getActionCommand() == "Dungeon")
               {
                   storyT.setText("You have entered the dungeon!");
-
               }
-
               else  if(eve.getActionCommand() == "Kitchen")
               {
                   storyT.setText("You have entered the kitchen!");
-
               }
-
               else if(eve.getActionCommand() == "Boiler")
               {
                   storyT.setText("You have entered the boiler!");
-
               }
               else  if(eve.getActionCommand() == "Bedroom")
               {
                   storyT.setText("You have entered the bedroom!");
-
               }
               else  if(eve.getActionCommand() == "Library")
               {
