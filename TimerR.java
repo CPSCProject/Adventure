d0994b8ebc31c3f322f3e5e99c49efb67485dc22
Rumika Mascarenhas <rumika.mascarenhas@ucalgary.ca>
2017-04-11 15:36:35 -0600

    new timer

--- TimerR.java
+++ TimerR.java
@@ -1,5 +1,7 @@
+package GUI;
 //@author Rumika Mascarenhas
 //Timer: Counts down time; variable count is changed in different rooms.
+
 import java.util.Scanner;
 
 import java.awt.event.*;
@@ -7,27 +9,24 @@ import java.awt.*;
 import javax.swing.*;
 
 
-public class TimerR extends JFrame {
+public class TimerR extends JPanel {
   JLabel promptLabel, timerLabel;
   JTextField tf;
   JButton button;
   Timer timer;
 
-  public TimerR(int new_count) {
-    setLayout(new GridLayout(2,2,5,5));
+  public TimerR() {
 
     timerLabel = new JLabel("Waiting...", SwingConstants.CENTER);
     add(timerLabel);
 
-    int count = new_count;
+    int count = 600;
     timerLabel.setText("Time left: " + count);
+
 
     TimeClass tc = new TimeClass(count);
     timer = new Timer(1000, tc);
     timer.start();
-    //event e = new event();
-    //button.addActionListener(e);1
-
 
   }
 
@@ -46,6 +45,7 @@ public class TimerR extends JFrame {
 
       if(counter >= 1) {
         timerLabel.setText("Time left: " + counter);
+
       }
       else {
         timer.stop();
@@ -55,10 +55,5 @@ public class TimerR extends JFrame {
   }
 
 
-    //TimerR gui = new TimerR();
-    //gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //gui.setSize(251,100);
-    //gui.setTitle("Timer Program");
-    //gui.setVisible(true);
 
 }
